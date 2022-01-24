fd8d02ea1b4a4196b61baf1047a3a8d05f8ecd7c
britton-sn1 <neilcbritton@hotmail.com>
2022-01-24 16:23:57 +0000

    removed debug.bat

--- DietLogger/debug.bat
+++
@@ -1,1 +0,0 @@
-mvn spring-boot:run -Dspring-boot.run.jvmArguments="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8000" -Dspring-boot.run.arguments="--spring.datasource.password=Harlosh123"
